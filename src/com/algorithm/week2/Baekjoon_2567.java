aab68e2a64218f1d9a7ffbffda48052cc91562ef
SEONMI <gus_tjs_al@naver.com>
2021-02-15 21:42:02 +0900

    update 색종이2

--- src/com/algorithm/week2/Baekjoon_2567.java
+++ src/com/algorithm/week2/Baekjoon_2567.java
@@ -3,50 +3,39 @@ package com.algorithm.week2;
 import java.util.Scanner;
 
 public class Baekjoon_2567 {
-	static int N;
-	static boolean[][] check;
-	static int[] dr = { 0, 1, 0,-1 }; // 우하좌상
-	static int[] dc = { 1, 0, -1 ,0};
+	static int T, flag;
+	static int[][] map;
+	static int[] dr = { 1, 0, -1, 0 }; // 하 우 상 좌
+	static int[] dc = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		check = new boolean[101][101];
-		int length = 0;
-		for (int i = 0; i < N; i++) {
+		map = new int[102][102];
+		T = sc.nextInt();
+		int ans = 0;
+		for (int t = 0; t < T; t++) {
 			int r = sc.nextInt();
 			int c = sc.nextInt();
-			length += go(r, c, 0);
-		}
-		System.out.println(length);
-		sc.close();
-	}
-
-	private static int go(int r, int c, int ans) {
-		int flag = 0;// 안겹침 +
-		int sr = r, sc = c;
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 10; j++) {
-				r = r + dr[i];
-				c = c + dc[i];
-				if (flag == 0)
-					ans++;
-				else
-					ans--;
-		
-				// + -를 바꿈
-				if (check[r][c]) {
-					if (flag == 0) {
-						flag = 1;
-					} else {
-						flag = 0;
-					}
-				}else {
-					check[r][c] = true;
+			// 색종이 영역 표시
+			for (int i = r; i < r + 10; i++) {
+				for (int j = c; j < c + 10; j++) {
+					map[i][j] = 1;
 				}
 			}
 		}
-		return ans;
+		for (int i = 1; i <= 100; i++) {
+			for (int j = 1; j <= 100; j++) {
+				// 색종이영역의
+				if(map[i][j] == 1) {
+					// 상하좌우 확인
+					if(map[i-1][j]==0) ans++;
+					if(map[i+1][j]==0) ans++;
+					if(map[i][j-1]==0) ans++;
+					if(map[i][j+1]==0) ans++;
+				}
+			}
+		}
+		System.out.println(ans);
 	}
 
 }

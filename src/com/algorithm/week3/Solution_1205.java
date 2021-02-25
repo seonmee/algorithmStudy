6aeb729e0581f519c0e50a75e6d5611a14729b2b
SEONMI <gus_tjs_al@naver.com>
2021-02-25 17:59:55 +0900

    Update 현선미/정올/1205_조커

--- src/com/algorithm/week3/Solution_1205.java
+++ src/com/algorithm/week3/Solution_1205.java
@@ -1,47 +1,52 @@
 package com.algorithm.week3;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
+// 정올_조커
 public class Solution_1205 {
-	static int N,zok, ans = Integer.MIN_VALUE;
-	static ArrayList<Integer> arr;
+	static int N, jok;
+	static ArrayList<Integer> cards;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// 입력
 		N = sc.nextInt();
-		arr = new ArrayList<>();
+		cards = new ArrayList<>();
+		int num = 0;
 		for (int i = 0; i < N; i++) {
-			int num = sc.nextInt();
-			if (num == 0) {
-				zok++;
-			} else {
-				arr.add(num);
-			}
+			num = sc.nextInt();
+			if (num == 0)
+				jok++;
+			else
+				cards.add(num);
 		}
-		arr.sort(null);
-		checkArr();
-		System.out.println(ans);
-	}
-
-	private static void checkArr() {
-		int cnt = 1;
-		for (int i = 0; i < arr.size() - 1; i++) {
-			int minus = arr.get(i + 1) - arr.get(i);
-			if (minus == 1) {
-				cnt++;
-			} else {
-				if (zok!=0 && zok >= minus -1) {
-					cnt++;
-					for (int j = 0; j < minus-1; j++) {
-						zok--;
-						cnt++;
-					}
+		// 로직
+		// 오름차순 정렬
+		cards.sort((Integer o1, Integer o2)-> {
+				return o1-o2;
+		});
+		int ans = 0;
+		for (int i = 0; i < cards.size(); i++) {
+			int len = 1;
+			for (int j = i; j < cards.size()-1; j++) {
+				// 연속적인 숫자가 아닌 경우
+				if(cards.get(j) + 1 != cards.get(j+1)) {
+					if(jok!=0) { // 조커가 있으면
+						jok--;
+						len++;
+					}else
+						break;
 				}else {
-					ans = Math.max(ans, cnt + zok);			
+					len++;
 				}
 			}
+			ans = Math.max(len, ans);
 		}
+		// 출력
+		System.out.println(ans);
+
 	}
 
 }

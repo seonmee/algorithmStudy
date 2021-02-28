9e19f8a0f0fe8ce286eed0e2f8714eafe76bf81e
SEONMI <gus_tjs_al@naver.com>
2021-02-28 13:58:14 +0900

    Update 현선미/백준/1337_버블소트 After Code Review

--- src/com/algorithm/week4/common/Solution_1377_v2.java
+++ src/com/algorithm/week4/common/Solution_1377_v2.java
@@ -9,30 +9,36 @@ import java.util.Map;
 
 public class Solution_1377_v2 {
 	static int N;
-	static Map A;
+	static Map A, B;
 	static int[] temp;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		A = new HashMap<Integer, Integer>();
+		B = new HashMap<Integer, Integer>(); // 정렬 이전
 		temp = new int[N];
 		for (int n = 0; n < N; n++) {
 			temp[n] = Integer.parseInt(br.readLine());
-			A.put(temp[n], n);
+			B.put(temp[n], n);
 		}
 		// 로직
 		Arrays.sort(temp);
+		A = new HashMap<Integer, Integer>(); // 정렬 이후
+		for (int n = 0; n < N; n++) {
+			A.put(temp[n], n);
+		}
+		
 		int[] idx = new int[N];
 		int max = Integer.MIN_VALUE;
 		int ans = 0;
+		
 		for (int n = 0; n < N; n++) {
-			idx[n] = (int) A.get(temp[n]) - n;
+			idx[n] = (int)B.get(temp[n]) - (int) A.get(temp[n]);
 			if (idx[n] >= 0) {
 				if (idx[n] >= max) {
 					max = idx[n];
-					ans = temp[n];
+					ans = max + 1;
 				}
 			}
 		}

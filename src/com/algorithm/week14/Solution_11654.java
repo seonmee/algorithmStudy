b9fde91b69d3dca4bf783c4248abfd4a67dd6838
SEONMI <gus_tjs_al@naver.com>
2021-04-29 23:13:20 +0900

    Update 현선미/백준/11654_아스키코드

---
+++ src/com/algorithm/week14/Solution_11654.java
@@ -0,0 +1,14 @@
+package com.algorithm.week14;
+
+import java.util.Scanner;
+// 아스키 코드 
+public class Solution_11654 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char ans = sc.nextLine().charAt(0);
+		sc.close();
+		System.out.println((int)ans);
+	}
+
+}

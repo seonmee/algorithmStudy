287f13e40ea9b77979a4527c08a9fe8583e50429
seonmee <gus_tjs_al@naver.com>
2021-07-09 00:30:43 +0900

    Update 현선미/백준/2193_이친수

---
+++ src/com/algorithm/week24/Solution_2193.java
@@ -0,0 +1,22 @@
+package com.algorithm.week24;
+import java.util.*;
+/*
+ * 이친수 
+ * */
+public class Solution_2193 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		long d[] = new long[n+1];
+		
+		d[0] = 0; d[1] = 1;
+		
+		for(int i=2;i<=n;i++){
+			d[i] = d[i-2]+d[i-1];
+		}
+		System.out.println(d[n]);
+	}
+}

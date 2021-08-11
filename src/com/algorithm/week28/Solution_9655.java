d587d9a0deb39e103be1073f6789428bdc815750
seonmee <gus_tjs_al@naver.com>
2021-08-11 21:54:24 +0900

    Update 현선미/백준/9655_돌게임

--- src/com/algorithm/week28/Solution_9655.java
+++ src/com/algorithm/week28/Solution_9655.java
@@ -1,10 +1,34 @@
 package com.algorithm.week28;
+import java.util.*;
+/*
+ * 돌게임
+ * 
+ * 탁자 위에 돌 N개가 있다. 
+ * 상근이와 창영이는 턴을 번갈아가면서 돌을 가져가며, 
+ * 돌은 1개 또는 3개 가져갈 수 있다. 
+ * 마지막 돌을 가져가는 사람이 게임을 이기게 된다.
+ * 
+ * 1 > 상근
+ * 2 > 창영
+ * 3 > 1 1 1 상근 
+ * 4 > 3 1 창영 
+ * 
+ * 홀수 상근 
+ * 짝수 창영
+ * 
+ * 엥?
+ * 
+ * */
 
 public class Solution_9655 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if(N%2==0) System.out.println("CY");
+		else System.out.println("SK");
 	}
 
 }

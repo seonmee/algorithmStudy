2b20ee0465d5d3b8042e319d4f537a4e4d8ccac4
seonmee <gus_tjs_al@naver.com>
2021-08-25 22:12:02 +0900

    Update 현선미/백준/19637_IF문 좀 대신 써줘

---
+++ src/com/algorithm/week30/Solution_19637.java
@@ -0,0 +1,60 @@
+package com.algorithm.week30;
+
+import java.util.*;
+import java.io.*;
+
+/* 
+ * IF문 좀 대신 써줘 
+ * */
+public class Solution_19637 {
+	static int N, M;
+
+	static class level {
+		String name;
+		int num;
+
+		public level(String name, int num) {
+			super();
+			this.name = name;
+			this.num = num;
+		}
+	}
+
+	static ArrayList<level> levels;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		levels = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int num = Integer.parseInt(st.nextToken());
+			levels.add(new level(name, num));
+		}
+
+		for (int i = 0; i < M; i++) {
+			int input = Integer.parseInt(br.readLine());
+			System.out.println(solve(input));
+		}
+	}
+
+	private static String solve(int input) {
+		int start = 0, end = levels.size() - 1, mid = 0;
+		
+		while (start <= end) {
+			mid = (start + end) / 2;
+			if (input > levels.get(mid).num)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+		return levels.get(end + 1).name;
+
+	}
+
+}
